ae932f3afb4c8e8ee32d5e01832967f7469d273f
Matt Astill <matt.astill@tag-group.com>
2026-04-30 16:51:52 +0100

    Retrying global tags

--- SmingCode.Utilities.Logging.AspNetCore/Injection.cs
+++ SmingCode.Utilities.Logging.AspNetCore/Injection.cs
@@ -2,8 +2,12 @@
 using Microsoft.Extensions.DependencyInjection;
 using Azure.Monitor.OpenTelemetry.AspNetCore;
 using Microsoft.Extensions.Logging;
+using OpenTelemetry.Trace;
+using OpenTelemetry;
+using System.Diagnostics;
 
 namespace SmingCode.Utilities.Logging.AspNetCore;
+using ServiceMetadata;
 
 public static class Injection
 {
@@ -12,10 +16,24 @@ public static class Injection
     )
     {
         builder.Services.AddOpenTelemetry()
-            .UseAzureMonitor();
+            .UseAzureMonitor()
+            .WithTracing(builder => builder.AddProcessor<ServiceMetadataOpenTelemetryActivityEnrichingProcessor>());
 
         builder.Logging.AddOpenTelemetry(options => options.IncludeScopes = true);
 
         return builder;
+    }
+}
+
+internal class ServiceMetadataOpenTelemetryActivityEnrichingProcessor(
+    IServiceMetadataProvider _serviceMetadataProvider
+) : BaseProcessor<Activity>
+{
+    public override void OnEnd(Activity activity)
+    {
+        foreach (var serviceMetadataDimension in _serviceMetadataProvider.GetMetadata().GetCustomDimensions())
+        {
+            activity.SetTag(serviceMetadataDimension.Key, serviceMetadataDimension.Value);
+        }
     }
 }
